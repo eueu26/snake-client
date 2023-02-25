5b66ce35d8ae9ff49f8fb06c495d14df9b1dabd1
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-25 18:01:39 -0500

    sending data

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+const connect = require("./client");
 const stdin = process.stdin;
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -11,12 +15,23 @@ const setupInput = function() {
 const handleUserInput = function() {
   // your code here
   stdin.on("data", (data) => {
-    console.log("data clicked: ", data);
+    // console.log("data clicked: ", data);
     if (data === "\u0003") {
       process.exit();
-    } else {
-      return data;
     }
+    if (data === 'w') {
+      stdin.write("Move: up\n");
+    }
+    if (data === 'a') {
+      stdin.write("Move: left\n");
+    }
+    if (data === 's') {
+      stdin.write("Move: down\n");
+    }
+    if (data === 'd') {
+      stdin.write("Move: right\n");
+    }
+    return data;
   });
 };
 module.exports = setupInput;
